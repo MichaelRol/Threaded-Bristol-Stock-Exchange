d1dfb9b66214f588a3791d7fa66f2347999e364a
Michael Rollins <michael.rollins@hotmail.co.uk>
2020-04-23 18:13:33 +0100

    Bit of cleaning up.

--- RSE.py
+++ RSE.py
@@ -6,7 +6,7 @@ import queue
 import random
 from RSE_exchange import Exchange
 from RSE_customer_orders import customer_orders
-from RSE_trader_agents import Trader_Giveaway, Trader_Shaver, Trader_Sniper, Trader_ZIC, Trader_ZIP, Trader_ZIP2
+from RSE_trader_agents import Trader_Giveaway, Trader_Shaver, Trader_Sniper, Trader_ZIC, Trader_ZIP
 
 
 # trade_stats()
@@ -20,30 +20,30 @@ def trade_stats(expid, traders, dumpfile, time, lob):
 	trader_types = {}
 	# n_traders = len(traders)
 	for t in traders:
-			ttype = traders[t].ttype
-			if ttype in trader_types.keys():
-					t_balance = trader_types[ttype]['balance_sum'] + traders[t].balance
-					n = trader_types[ttype]['n'] + 1
-			else:
-					t_balance = traders[t].balance
-					n = 1
-			trader_types[ttype] = {'n':n, 'balance_sum':t_balance}
+		ttype = traders[t].ttype
+		if ttype in trader_types.keys():
+			t_balance = trader_types[ttype]['balance_sum'] + traders[t].balance
+			n = trader_types[ttype]['n'] + 1
+		else:
+			t_balance = traders[t].balance
+			n = 1
+		trader_types[ttype] = {'n':n, 'balance_sum':t_balance}
 
 
 	dumpfile.write('%s, %06d, ' % (expid, time))
 	for ttype in sorted(list(trader_types.keys())):
-			n = trader_types[ttype]['n']
-			s = trader_types[ttype]['balance_sum']
-			dumpfile.write('%s, %d, %d, %f, ' % (ttype, s, n, s / float(n)))
+		n = trader_types[ttype]['n']
+		s = trader_types[ttype]['balance_sum']
+		dumpfile.write('%s, %d, %d, %f, ' % (ttype, s, n, s / float(n)))
 
 	if lob['bids']['best'] != None :
-			dumpfile.write('%d, ' % (lob['bids']['best']))
+		dumpfile.write('%d, ' % (lob['bids']['best']))
 	else:
-			dumpfile.write('N, ')
+		dumpfile.write('N, ')
 	if lob['asks']['best'] != None :
-			dumpfile.write('%d, ' % (lob['asks']['best']))
+		dumpfile.write('%d, ' % (lob['asks']['best']))
 	else:
-			dumpfile.write('N, ')
+		dumpfile.write('N, ')
 	dumpfile.write('\n')
 
 
@@ -56,67 +56,67 @@ def trade_stats(expid, traders, dumpfile, time, lob):
 def populate_market(traders_spec, traders, shuffle, verbose):
 
 	def trader_type(robottype, name):
-			if robottype == 'GVWY':
-					return Trader_Giveaway('GVWY', name, 0.00, 0)
-			elif robottype == 'ZIC':
-					return Trader_ZIC('ZIC', name, 0.00, 0)
-			elif robottype == 'SHVR':
-					return Trader_Shaver('SHVR', name, 0.00, 0)
-			elif robottype == 'SNPR':
-					return Trader_Sniper('SNPR', name, 0.00, 0)
-			elif robottype == 'ZIP':
-					return Trader_ZIP2('ZIP', name, 0.00, 0)
-			else:
-					sys.exit('FATAL: don\'t know robot type %s\n' % robottype)
+		if robottype == 'GVWY':
+			return Trader_Giveaway('GVWY', name, 0.00, 0)
+		elif robottype == 'ZIC':
+			return Trader_ZIC('ZIC', name, 0.00, 0)
+		elif robottype == 'SHVR':
+			return Trader_Shaver('SHVR', name, 0.00, 0)
+		elif robottype == 'SNPR':
+			return Trader_Sniper('SNPR', name, 0.00, 0)
+		elif robottype == 'ZIP':
+			return Trader_ZIP('ZIP', name, 0.00, 0)
+		else:
+			sys.exit('FATAL: don\'t know robot type %s\n' % robottype)
 
 
 	def shuffle_traders(ttype_char, n, traders):
-			for swap in range(n):
-					t1 = (n - 1) - swap
-					t2 = random.randint(0, t1)
-					t1name = '%c%02d' % (ttype_char, t1)
-					t2name = '%c%02d' % (ttype_char, t2)
-					traders[t1name].tid = t2name
-					traders[t2name].tid = t1name
-					temp = traders[t1name]
-					traders[t1name] = traders[t2name]
-					traders[t2name] = temp
+		for swap in range(n):
+			t1 = (n - 1) - swap
+			t2 = random.randint(0, t1)
+			t1name = '%c%02d' % (ttype_char, t1)
+			t2name = '%c%02d' % (ttype_char, t2)
+			traders[t1name].tid = t2name
+			traders[t2name].tid = t1name
+			temp = traders[t1name]
+			traders[t1name] = traders[t2name]
+			traders[t2name] = temp
 
 
 	n_buyers = 0
 	for bs in traders_spec['buyers']:
-			ttype = bs[0]
-			for _ in range(bs[1]):
-					tname = 'B%02d' % n_buyers  # buyer i.d. string
-					traders[tname] = trader_type(ttype, tname)
-					n_buyers = n_buyers + 1
+		ttype = bs[0]
+		for _ in range(bs[1]):
+			tname = 'B%02d' % n_buyers  # buyer i.d. string
+			traders[tname] = trader_type(ttype, tname)
+			n_buyers = n_buyers + 1
 
 	if n_buyers < 1:
-			sys.exit('FATAL: no buyers specified\n')
+		sys.exit('FATAL: no buyers specified\n')
 
 	if shuffle: shuffle_traders('B', n_buyers, traders)
 
 
 	n_sellers = 0
 	for ss in traders_spec['sellers']:
-			ttype = ss[0]
-			for _ in range(ss[1]):
-					tname = 'S%02d' % n_sellers  # buyer i.d. string
-					traders[tname] = trader_type(ttype, tname)
-					n_sellers = n_sellers + 1
+		ttype = ss[0]
+		for _ in range(ss[1]):
+			tname = 'S%02d' % n_sellers  # buyer i.d. string
+			traders[tname] = trader_type(ttype, tname)
+			n_sellers = n_sellers + 1
 
 	if n_sellers < 1:
-			sys.exit('FATAL: no sellers specified\n')
+		sys.exit('FATAL: no sellers specified\n')
 
 	if shuffle: shuffle_traders('S', n_sellers, traders)
 
 	if verbose :
-			for t in range(n_buyers):
-					bname = 'B%02d' % t
-					print(traders[bname])
-			for t in range(n_sellers):
-					bname = 'S%02d' % t
-					print(traders[bname])
+		for t in range(n_buyers):
+			bname = 'B%02d' % t
+			print(traders[bname])
+		for t in range(n_sellers):
+			bname = 'S%02d' % t
+			print(traders[bname])
 
 
 	return {'n_buyers':n_buyers, 'n_sellers':n_sellers}
@@ -138,6 +138,7 @@ def run_exchange(exchange, order_q, trader_qs, start_event, start_time, sess_len
 		trade = exchange.process_order2(virtual_time, order, process_verbose)
 		
 		if trade is not None:
+			print(trade)
 			completed_coid[order.coid] = True
 			completed_coid[trade['counter']] = True
 			for q in trader_qs:
@@ -149,7 +150,7 @@ def run_trader(trader, exchange, order_q, trader_q, start_event, start_time, ses
 	start_event.wait()
 	
 	while start_event.isSet():
-		time.sleep(0.04)
+		time.sleep(0.01)
 		virtual_time = (time.time() - start_time) * (virtual_end / sess_length)
 		time_left =  (virtual_end - virtual_time) / virtual_end
 
@@ -227,12 +228,12 @@ def market_session(sess_id, sess_length, virtual_end, trader_spec, order_schedul
 											order_schedule, pending_cust_orders, orders_verbose)
 		# if any newly-issued customer orders mean quotes on the LOB need to be cancelled, kill them
 		if len(kills) > 0 :
-				# if verbose : print('Kills: %s' % (kills))
-				for kill in kills :
-						# if verbose : print('lastquote=%s' % traders[kill].lastquote)
-						if traders[kill].lastquote != None :
-								# if verbose : print('Killing order %s' % (str(traders[kill].lastquote)))
-								exchange.del_order(virtual_time, traders[kill].lastquote, verbose)
+			# if verbose : print('Kills: %s' % (kills))
+			for kill in kills :
+				# if verbose : print('lastquote=%s' % traders[kill].lastquote)
+				if traders[kill].lastquote != None :
+					# if verbose : print('Killing order %s' % (str(traders[kill].lastquote)))
+					exchange.del_order(virtual_time, traders[kill].lastquote, verbose)
 
 	# print("QUEUE: " + str(order_q.qsize()))
 	start_event.clear()
@@ -253,19 +254,19 @@ def market_session(sess_id, sess_length, virtual_end, trader_spec, order_schedul
 if __name__ == "__main__":
 
 	# set up parameters for the session
-	sess_length = 10.0 # Length of the session in seconds
+	sess_length = 5.0 # Length of the session in seconds
 	virtual_end = 600 # Number of virtual seconds for each session
 
 
 	# schedule_offsetfn returns time-dependent offset on schedule prices
 	def schedule_offsetfn(t):
-			pi2 = math.pi * 2
-			c = math.pi * 3000
-			wavelength = t / c
-			gradient = 100 * t / (c / pi2)
-			amplitude = 100 * t / (c / pi2)
-			offset = gradient + amplitude * math.sin(wavelength * t)
-			return int(round(offset, 0))
+		pi2 = math.pi * 2
+		c = math.pi * 3000
+		wavelength = t / c
+		gradient = 100 * t / (c / pi2)
+		amplitude = 100 * t / (c / pi2)
+		offset = gradient + amplitude * math.sin(wavelength * t)
+		return int(round(offset, 0))
 			
 
 	range1 = (95, 95, schedule_offsetfn)
@@ -292,16 +293,16 @@ if __name__ == "__main__":
 	tdump=open('avg_balance.csv','w')
 	trial = 1
 	if n_trials > 1:
-			dump_all = False
+		dump_all = False
 	else:
-			dump_all = True
+		dump_all = True
 			
 	while (trial<(n_trials+1)):
-			trial_id = 'trial%04d' % trial
-			market_session(trial_id, sess_length, virtual_end, traders_spec,
-											order_sched, tdump, False, True)
-			tdump.flush()
-			trial = trial + 1
+		trial_id = 'trial%04d' % trial
+		market_session(trial_id, sess_length, virtual_end, traders_spec,
+										order_sched, tdump, False, True)
+		tdump.flush()
+		trial = trial + 1
 	tdump.close()
 
 	sys.exit('Done Now')
--- RSE_customer_orders.py
+++ RSE_customer_orders.py
@@ -7,121 +7,122 @@ def customer_orders(time, cuid, last_update, traders, trader_stats, os, pending,
 
 
         def sysmin_check(price):
-                if price < rse_sys_minprice:
-                        print('WARNING: price < bse_sys_min -- clipped')
-                        price = rse_sys_minprice
-                return price
+            if price < rse_sys_minprice:
+                print('WARNING: price < bse_sys_min -- clipped')
+                price = rse_sys_minprice
+            return price
 
 
         def sysmax_check(price):
-                if price > rse_sys_maxprice:
-                        print('WARNING: price > bse_sys_max -- clipped')
-                        price = rse_sys_maxprice
-                return price
+            if price > rse_sys_maxprice:
+                print('WARNING: price > bse_sys_max -- clipped')
+                price = rse_sys_maxprice
+            return price
 
         
 
         def getorderprice(i, sched, n, mode, issuetime):
-                # does the first schedule range include optional dynamic offset function(s)?
-                if len(sched[0]) > 2:
-                        offsetfn = sched[0][2]
-                        if callable(offsetfn):
-                                # same offset for min and max
-                                offset_min = offsetfn(issuetime)
-                                offset_max = offset_min
-                        else:
-                                sys.exit('FAIL: 3rd argument of sched in getorderprice() not callable')
-                        if len(sched[0]) > 3:
-                                # if second offset function is specfied, that applies only to the max value
-                                offsetfn = sched[0][3]
-                                if callable(offsetfn):
-                                        # this function applies to max
-                                        offset_max = offsetfn(issuetime)
-                                else:
-                                        sys.exit('FAIL: 4th argument of sched in getorderprice() not callable')
+            # does the first schedule range include optional dynamic offset function(s)?
+            if len(sched[0]) > 2:
+                offsetfn = sched[0][2]
+                if callable(offsetfn):
+                    # same offset for min and max
+                    offset_min = offsetfn(issuetime)
+                    offset_max = offset_min
                 else:
-                        offset_min = 0.0
-                        offset_max = 0.0
+                    sys.exit('FAIL: 3rd argument of sched in getorderprice() not callable')
+                if len(sched[0]) > 3:
+                    # if second offset function is specfied, that applies only to the max value
+                    offsetfn = sched[0][3]
+                    if callable(offsetfn):
+                        # this function applies to max
+                        offset_max = offsetfn(issuetime)
+                    else:
+                        sys.exit('FAIL: 4th argument of sched in getorderprice() not callable')
+            else:
+                offset_min = 0.0
+                offset_max = 0.0
 
-                pmin = sysmin_check(offset_min + min(sched[0][0], sched[0][1]))
-                pmax = sysmax_check(offset_max + max(sched[0][0], sched[0][1]))
-                prange = pmax - pmin
-                stepsize = prange / (n - 1)
-                halfstep = round(stepsize / 2.0)
+            pmin = sysmin_check(offset_min + min(sched[0][0], sched[0][1]))
+            pmax = sysmax_check(offset_max + max(sched[0][0], sched[0][1]))
+            prange = pmax - pmin
+            stepsize = prange / (n - 1)
+            halfstep = round(stepsize / 2.0)
 
-                if mode == 'fixed':
-                        orderprice = pmin + int(i * stepsize) 
-                elif mode == 'jittered':
-                        orderprice = pmin + int(i * stepsize) + random.randint(-halfstep, halfstep)
-                elif mode == 'random':
-                        if len(sched) > 1:
-                                # more than one schedule: choose one equiprobably
-                                s = random.randint(0, len(sched) - 1)
-                                pmin = sysmin_check(min(sched[s][0], sched[s][1]))
-                                pmax = sysmax_check(max(sched[s][0], sched[s][1]))
-                        orderprice = random.randint(pmin, pmax)
-                else:
-                        sys.exit('FAIL: Unknown mode in schedule')
-                orderprice = sysmin_check(sysmax_check(orderprice))
-                return orderprice
+            if mode == 'fixed':
+                orderprice = pmin + int(i * stepsize) 
+            elif mode == 'jittered':
+                orderprice = pmin + int(i * stepsize) + random.randint(-halfstep, halfstep)
+            elif mode == 'random':
+                if len(sched) > 1:
+                    # more than one schedule: choose one equiprobably
+                    s = random.randint(0, len(sched) - 1)
+                    pmin = sysmin_check(min(sched[s][0], sched[s][1]))
+                    pmax = sysmax_check(max(sched[s][0], sched[s][1]))
+                orderprice = random.randint(pmin, pmax)
+            else:
+                sys.exit('FAIL: Unknown mode in schedule')
+            orderprice = sysmin_check(sysmax_check(orderprice))
+            return orderprice
 
 
 
         def getissuetimes(n_traders, mode, interval, shuffle, fittointerval):
-                interval = float(interval)
-                if n_traders < 1:
-                        sys.exit('FAIL: n_traders < 1 in getissuetime()')
-                elif n_traders == 1:
-                        tstep = interval
+
+            interval = float(interval)
+            if n_traders < 1:
+                sys.exit('FAIL: n_traders < 1 in getissuetime()')
+            elif n_traders == 1:
+                tstep = interval
+            else:
+                tstep = interval / (n_traders - 1)
+            arrtime = 0
+            issuetimes = []
+            for t in range(n_traders):
+                if mode == 'periodic':
+                    arrtime = interval
+                elif mode == 'drip-fixed':
+                    arrtime = t * tstep
+                elif mode == 'drip-jitter':
+                    arrtime = t * tstep + tstep * random.random()
+                elif mode == 'drip-poisson':
+                    # poisson requires a bit of extra work
+                    interarrivaltime = random.expovariate(n_traders / interval)
+                    arrtime += interarrivaltime
                 else:
-                        tstep = interval / (n_traders - 1)
-                arrtime = 0
-                issuetimes = []
+                    sys.exit('FAIL: unknown time-mode in getissuetimes()')
+                issuetimes.append(arrtime) 
+                
+            # at this point, arrtime is the last arrival time
+            if fittointerval and ((arrtime > interval) or (arrtime < interval)):
+                # generated sum of interarrival times longer than the interval
+                # squish them back so that last arrival falls at t=interval
                 for t in range(n_traders):
-                        if mode == 'periodic':
-                                arrtime = interval
-                        elif mode == 'drip-fixed':
-                                arrtime = t * tstep
-                        elif mode == 'drip-jitter':
-                                arrtime = t * tstep + tstep * random.random()
-                        elif mode == 'drip-poisson':
-                                # poisson requires a bit of extra work
-                                interarrivaltime = random.expovariate(n_traders / interval)
-                                arrtime += interarrivaltime
-                        else:
-                                sys.exit('FAIL: unknown time-mode in getissuetimes()')
-                        issuetimes.append(arrtime) 
-                        
-                # at this point, arrtime is the last arrival time
-                if fittointerval and ((arrtime > interval) or (arrtime < interval)):
-                        # generated sum of interarrival times longer than the interval
-                        # squish them back so that last arrival falls at t=interval
-                        for t in range(n_traders):
-                                issuetimes[t] = interval * (issuetimes[t] / arrtime)
-                # optionally randomly shuffle the times
-                if shuffle:
-                        for t in range(n_traders):
-                                i = (n_traders - 1) - t
-                                j = random.randint(0, i)
-                                tmp = issuetimes[i]
-                                issuetimes[i] = issuetimes[j]
-                                issuetimes[j] = tmp
-                return issuetimes
+                    issuetimes[t] = interval * (issuetimes[t] / arrtime)
+            # optionally randomly shuffle the times
+            if shuffle:
+                for t in range(n_traders):
+                    i = (n_traders - 1) - t
+                    j = random.randint(0, i)
+                    tmp = issuetimes[i]
+                    issuetimes[i] = issuetimes[j]
+                    issuetimes[j] = tmp
+            return issuetimes
         
 
         def getschedmode(time, os):
-                got_one = False
-                for sched in os:
-                        if (sched['from'] <= time) and (time < sched['to']) :
-                                # within the timezone for this schedule
-                                schedrange = sched['ranges']
-                                mode = sched['stepmode']
-                                got_one = True
-                                exit  # jump out the loop -- so the first matching timezone has priority over any others
-                if not got_one:
-                        sys.exit('Fail: time=%5.2f not within any timezone in os=%s' % (time, os))
-                return (schedrange, mode)
-        
+            got_one = False
+            for sched in os:
+                if (sched['from'] <= time) and (time < sched['to']) :
+                    # within the timezone for this schedule
+                    schedrange = sched['ranges']
+                    mode = sched['stepmode']
+                    got_one = True
+                    exit  # jump out the loop -- so the first matching timezone has priority over any others
+            if not got_one:
+                sys.exit('Fail: time=%5.2f not within any timezone in os=%s' % (time, os))
+            return (schedrange, mode)
+    
 
         n_buyers = trader_stats['n_buyers']
         n_sellers = trader_stats['n_sellers']
@@ -131,52 +132,49 @@ def customer_orders(time, cuid, last_update, traders, trader_stats, os, pending,
         cancellations = []
 
         if len(pending) < 1:
-                # print("NEW CUST ORDER")
-                # list of pending (to-be-issued) customer orders is empty, so generate a new one
-                new_pending = []
+            # list of pending (to-be-issued) customer orders is empty, so generate a new one
+            new_pending = []
 
-                # demand side (buyers)
-                issuetimes = getissuetimes(n_buyers, os['timemode'], os['interval'], shuffle_times, True)
-                
-                ordertype = 'Bid'
-                (sched, mode) = getschedmode(time, os['dem'])             
-                for t in range(n_buyers):
-                        issuetime = time + issuetimes[t]
-                        tname = 'B%02d' % t
-                        orderprice = getorderprice(t, sched, n_buyers, mode, issuetime)
-                        order = Order(tname, ordertype, orderprice, 1, issuetime, cuid, -3.14)
-                        new_pending.append(order) 
-                        cuid += 1
-                        
-                # supply side (sellers)
-                issuetimes = getissuetimes(n_sellers, os['timemode'], os['interval'], shuffle_times, True)
-                ordertype = 'Ask'
-                (sched, mode) = getschedmode(time, os['sup'])
-                for t in range(n_sellers):
-                        issuetime = time + issuetimes[t]
-                        tname = 'S%02d' % t
-                        orderprice = getorderprice(t, sched, n_sellers, mode, issuetime)
-                        order = Order(tname, ordertype, orderprice, 1, issuetime, cuid, -3.14)
-                        new_pending.append(order)
-                        cuid += 1
+            # demand side (buyers)
+            issuetimes = getissuetimes(n_buyers, os['timemode'], os['interval'], shuffle_times, True)
+            
+            ordertype = 'Bid'
+            (sched, mode) = getschedmode(time, os['dem'])             
+            for t in range(n_buyers):
+                issuetime = time + issuetimes[t]
+                tname = 'B%02d' % t
+                orderprice = getorderprice(t, sched, n_buyers, mode, issuetime)
+                order = Order(tname, ordertype, orderprice, 1, issuetime, cuid, -3.14)
+                new_pending.append(order) 
+                cuid += 1
+                    
+            # supply side (sellers)
+            issuetimes = getissuetimes(n_sellers, os['timemode'], os['interval'], shuffle_times, True)
+            ordertype = 'Ask'
+            (sched, mode) = getschedmode(time, os['sup'])
+            for t in range(n_sellers):
+                issuetime = time + issuetimes[t]
+                tname = 'S%02d' % t
+                orderprice = getorderprice(t, sched, n_sellers, mode, issuetime)
+                order = Order(tname, ordertype, orderprice, 1, issuetime, cuid, -3.14)
+                new_pending.append(order)
+                cuid += 1
         else:
-
-                # print("PENDING ORDER")  
-                # there are pending future orders: issue any whose timestamp is in the past
-                new_pending = []
-                for order in pending:
-                        if order.time < time:
-                                # this order should have been issued by now
-                                # issue it to the trader
-                                tname = order.tid
-                                response = traders[tname].add_order(order, verbose)
-                                if verbose: print('Customer order: %s %s' % (response, order) )
-                                if response == 'LOB_Cancel' :
-                                    cancellations.append(tname)
-                                    if verbose: print('Cancellations: %s' % (cancellations))
-                                # and then don't add it to new_pending (i.e., delete it)
-                        else:
-                                # this order stays on the pending list
-                                new_pending.append(order)
+            # there are pending future orders: issue any whose timestamp is in the past
+            new_pending = []
+            for order in pending:
+                if order.time < time:
+                    # this order should have been issued by now
+                    # issue it to the trader
+                    tname = order.tid
+                    response = traders[tname].add_order(order, verbose)
+                    if verbose: print('Customer order: %s %s' % (response, order) )
+                    if response == 'LOB_Cancel' :
+                        cancellations.append(tname)
+                        if verbose: print('Cancellations: %s' % (cancellations))
+                    # and then don't add it to new_pending (i.e., delete it)
+                else:
+                    # this order stays on the pending list
+                    new_pending.append(order)
         return [new_pending, cancellations, cuid]
 
