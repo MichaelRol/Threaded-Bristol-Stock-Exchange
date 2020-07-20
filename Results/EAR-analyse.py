2407e7726595630a7e582eaafa2b06ace63d16ee
MichaelRol <michael.rollins@hotmail.co.uk>
2020-07-20 17:38:45 +0100

    More results analyse

--- Results/EAR-analyse.py
+++ Results/EAR-analyse.py
@@ -2,21 +2,21 @@ import csv
 
 ratios = []
 
-output = open('tear-output.csv', 'w')
+output = open('bear-output.csv', 'w')
 for i in range(1, 20):
 	name = ['00', '00']
 	name[0] = str(i).zfill(2)
 	name[1] = str(20-i).zfill(2)
-	with open('TEAR-'+name[0]+'-'+name[1]+'.csv') as testfile:
+	with open('EAR-'+name[0]+'-'+name[1]+'.csv') as testfile:
 		newreader = csv.reader(testfile, delimiter=',')
 		first = 0
 		second = 0
 		rows = list(newreader)
 		firstname = rows[0][1]
-		secondname = rows[0][9]
+		secondname = rows[0][8]
 		for row in rows:
 			# print(row)
-			if float(row[4]) > float(row[12]):
+			if float(row[4]) > float(row[11]):
 				first += 1
 			else:
 				second += 1
